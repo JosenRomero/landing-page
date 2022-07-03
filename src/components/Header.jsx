bd9a7ee7e69b923a978bab26fb2e74eb9586b771
Josen Romero <romerojosemanuel@hotmail.com>
2022-07-02 21:12:18 -0600

    update Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
-import { Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import Text from './Text'
 import CustomContainer from './CustomContainer'
 
 const Header = ({ data }) => {
   return (
-    <CustomContainer as={'header'} id={'home'} className={'container'}>
-      <Row>
-        <Col md={8} className="text-center mx-auto">
-          <Text as={'h1'}>{data.title}</Text>
-          <Text as={'p'}>{data.paragraph}</Text>
-        </Col>
-      </Row>
+    <CustomContainer as={'header'} id={'home'} className={'bg-light py-5'}>
+      <Container>
+        <Row>
+          <Col lg={4} className={'mb-3 d-grid align-content-center'}>
+            <Text as={'h1'}>{data.title}</Text>
+            <Text as={'p'}>{data.paragraph}</Text>
+          </Col>
+          <Col lg={6} className={'offset-lg-2'}>
+            <Image src={`img/${data.img}`} />
+          </Col>
+        </Row>
+      </Container>
     </CustomContainer>
   )
 }
